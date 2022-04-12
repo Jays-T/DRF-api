9964235f329572697e76da2bd00c980f6ba513a1
John Traas <j.j.h.traas@gmail.com>
2022-04-13 01:08:06 +0200

    Further update to CORS regexes to allow for gitpod updates to server ids

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -70,7 +70,7 @@ if 'CLIENT_ORIGIN' in os.environ:
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+.+\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 if 'LOCAL_DEV' in os.environ:
     CORS_ALLOWED_ORIGINS = [
