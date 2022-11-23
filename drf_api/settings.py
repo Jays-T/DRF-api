c08378ca698540fa4cc3d9692b71b32342bc0630
John Traas <j.j.h.traas@gmail.com>
2022-11-23 14:45:15 +0000

    deploy for render

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,6 +62,10 @@ ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
     'localhost',
 ]
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
